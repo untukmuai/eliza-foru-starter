57f8f2ab2c5485efbcba23b21880ea5a7b1089e1
Brixzen <brixzen@gmail.com>
2025-01-27 12:59:10 +0700

    fix db connection

--- src/models/index.ts
+++ src/models/index.ts
@@ -10,7 +10,21 @@ const __dirname = dirname(__filename);
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const configPath = path.join(__dirname, "..", "config", "config.json");
-const config = require(configPath)[env];
+
+async function loadConfig() {
+  try {
+    const data = fs.readFileSync(configPath, "utf8");
+    const config = JSON.parse(data);
+    const configForEnv = config[env];
+    console.log(configForEnv);
+    return configForEnv;
+  } catch (err) {
+    console.error("Failed to load config", err);
+    throw err;
+  }
+}
+
+const config = await loadConfig();
 const db: { [key: string]: any } = {};
 
 let sequelize: Sequelize;
@@ -35,9 +49,15 @@ fs.readdirSync(__dirname)
       file.indexOf(".test.ts") === -1
     );
   })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
-    db[model.name] = model;
+  .forEach(async (file) => {
+    try {
+      const modelPath = path.join(__dirname, file);
+      const modelModule = await import(modelPath); // Dynamic import
+      const model = modelModule.default(sequelize, DataTypes); // Call the model function
+      db[model.name] = model; // Add the model to the db object
+    } catch (err) {
+      console.error(`Failed to load model ${file}:`, err);
+    }  
   });
 
 Object.keys(db).forEach((modelName) => {
