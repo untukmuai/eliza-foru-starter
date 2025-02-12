3b0a4e3eeed6dbe1826a2a06db31dc3ca3a70b7c
alvinrach <alvinracht@gmail.com>
2025-02-12 09:43:29 +0700

    Endpoint: Personality to Character Migration

--- src/api/index.js
+++ src/api/index.js
@@ -33,6 +33,11 @@ import {
 import { REST, Routes } from "discord.js";
 import { stringToUuid } from "@elizaos/core";
 import db from "../models/index.ts";
+import OpenAI from 'openai'
+
+const client = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"],
+})
 
 const users = { developer: "foruweb3project@2024" };
 
@@ -96,6 +101,60 @@ function createApiRouter(agents, directClient) {
   });
 
   router.use(apiKeyMiddleware);
+
+  async function personalityToCharacter(theContent) {
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);
+    const char = await readFile(path.join(__dirname, 'character.json'), 'utf8');
+    const chatCompletion = await client.chat.completions.create({
+      response_format:{type:"json_object"},
+      messages: [
+        {
+        role:"system",
+        content: "You are a helpful assistant designed to process previous json to the subsequent given json format."
+        },
+        {
+        role:"user",
+        content: `
+        given an agent data that will be created inside a community in a json format we have
+        
+        Agent Name: ${theContent.agent_name}
+        Personality: ${theContent.agent_personality}
+        Agent's Traits: ${theContent.traits.join(' ')}
+        Agent's Tone of Voice: ${theContent.tone_voice.join(' ')}
+        Community Name: ${theContent.community_name}
+        Community Description: ${theContent.community_description}
+        
+        filled up this character.json values.
+        Use this format (this and the value only as example). 
+        ${char}
+        Generate 3-6 question-answer examples (messageExamples)
+        `
+        }
+      ],
+      model: "gpt-4o"
+    });
+    const responseText = chatCompletion.choices[0].message.content;
+    const mainResult = JSON.parse(responseText);
+    const additional = {
+      clients: ["direct"],
+      modelProvider: "openai",
+      plugins: []
+    };
+    const finalResult = { ...mainResult, ...additional};
+
+    return finalResult
+  };
+
+  router.post("/personality-to-character", async (req, res) => {
+    try{
+      const result = await personalityToCharacter(req.body);
+      res.json(result);
+    } catch (error) {
+      console.error("error bro:", error);
+      res.status(500).send("error processing request");
+    }
+  });
+
   router.get("/agents", (req, res) => {
     const agentsList = Array.from(agents.values()).map((agent) => ({
       id: agent.agentId,
