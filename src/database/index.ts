be3b5ec7a0821b3af41611a212d756d194b706dd
Brixzen <brixzen@gmail.com>
2025-02-26 14:39:53 +0700

    max con 2

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,7 @@ export function initializeDatabase() {
     const db = new PostgresDatabaseAdapter({
       connectionString: process.env.POSTGRES_URL,
       allowExitOnIdle: true,
-      max: 10,
+      max: 2,
       idleTimeoutMillis: 1000,
     });
     return db;
